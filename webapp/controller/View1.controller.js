ff5c5d7f32000cf237415d9216b0c4c13396d071
Sabrina <sabrinaheng115@gmail.com>
2026-01-28 19:18:37 +0800

    Second draft

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,86 +1,99 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
 ],
-function (Controller, JSONModel, MessageToast) {
-    "use strict";
+    function (Controller, JSONModel, MessageToast, MessageBox) {
+        "use strict";
 
-    return Controller.extend("project1.controller.View1", {
-        onInit: function () {
-            // Load Mock Data
-            var oModel = new JSONModel(sap.ui.require.toUrl("project1/model/data.json"));
-            this.getView().setModel(oModel, "courseModel");
-            
-            // Wait for data to load to run initial validation (though empty initially)
-            oModel.attachRequestCompleted(function() {
+        return Controller.extend("project1.controller.View1", {
+            onInit: function () {
+                // Load Mock Data
+                var oModel = new JSONModel(sap.ui.require.toUrl("project1/model/data.json"));
+                this.getView().setModel(oModel, "courseModel");
+
+                oModel.attachRequestCompleted(function () {
+                    this._validateCredits();
+                }.bind(this));
+            },
+
+            onAddCourse: function (oEvent) {
+                var oModel = this.getView().getModel("courseModel");
+                var oSource = oEvent.getSource();
+                var oContext = oSource.getBindingContext("courseModel");
+                var oSelectedCourse = oContext.getObject();
+
+                var aRegistered = oModel.getProperty("/registeredCourses");
+
+                // Check if already added
+                var bExists = aRegistered.some(function (course) {
+                    return course.code === oSelectedCourse.code;
+                });
+
+                if (bExists) {
+                    MessageToast.show("Course already added!");
+                    return;
+                }
+
+                aRegistered.push(oSelectedCourse);
+                oModel.setProperty("/registeredCourses", aRegistered);
+
                 this._validateCredits();
-            }.bind(this));
-        },
+            },
 
-        onAddCourse: function (oEvent) {
-            var oModel = this.getView().getModel("courseModel");
-            var oSource = oEvent.getSource();
-            var oContext = oSource.getBindingContext("courseModel");
-            var oSelectedCourse = oContext.getObject();
+            onRemoveCourse: function (oEvent) {
+                var oModel = this.getView().getModel("courseModel");
+                var oList = oEvent.getSource();
+                var oItem = oEvent.getParameter("listItem");
+                var sPath = oItem.getBindingContext("courseModel").getPath();
+                var iIndex = parseInt(sPath.split("/").pop());
 
-            var aRegistered = oModel.getProperty("/registeredCourses");
+                var aRegistered = oModel.getProperty("/registeredCourses");
+                aRegistered.splice(iIndex, 1);
 
-            // Check if already added
-            var bExists = aRegistered.some(function(course) {
-                return course.code === oSelectedCourse.code;
-            });
+                oModel.setProperty("/registeredCourses", aRegistered);
+                this._validateCredits();
+            },
 
-            if (bExists) {
-                MessageToast.show("Course already added!");
-                return;
+            onRegister: function () {
+                MessageBox.success("Course registration successful!");
+                // Optional: reset logic here if needed
+            },
+
+            _validateCredits: function () {
+                var oModel = this.getView().getModel("courseModel");
+                var aRegistered = oModel.getProperty("/registeredCourses");
+                var iTotalCredits = 0;
+
+                aRegistered.forEach(function (course) {
+                    iTotalCredits += course.credits;
+                });
+
+                oModel.setProperty("/ui/totalCredits", iTotalCredits);
+
+                var iMin = oModel.getProperty("/student/minCredits");
+                var iMax = oModel.getProperty("/student/maxCredits");
+
+                var bWarningVisible = false;
+                var sWarningMessage = "";
+                var sWarningType = "None";
+
+                if (iTotalCredits < iMin) {
+                    bWarningVisible = true;
+                    sWarningMessage = "Not enough Credit Hour !"; // Exact text from screenshot
+                    sWarningType = "Warning";
+                } else if (iTotalCredits > iMax) {
+                    bWarningVisible = true;
+                    sWarningMessage = "Credit Limit Exceeded !";
+                    sWarningType = "Error";
+                } else {
+                    bWarningVisible = false;
+                }
+
+                oModel.setProperty("/ui/isWarningVisible", bWarningVisible);
+                oModel.setProperty("/ui/warningMessage", sWarningMessage);
+                oModel.setProperty("/ui/warningType", sWarningType);
             }
-
-            // check if adding this course exceeds max credit limit
-            // Requirement says "Automatically show warning alerts", but usually we allow adding and show error, 
-            // OR we block adding. The screenshot shows "Not enough Credit Hour!" which implies < Min.
-            // If > Max, we should probably show "Exceed Credit Hour!".
-            // I'll allow adding and show the validation state.
-
-            aRegistered.push(oSelectedCourse);
-            oModel.setProperty("/registeredCourses", aRegistered);
-            
-            this._validateCredits();
-        },
-
-        _validateCredits: function() {
-            var oModel = this.getView().getModel("courseModel");
-            var aRegistered = oModel.getProperty("/registeredCourses");
-            var iTotalCredits = 0;
-
-            aRegistered.forEach(function(course) {
-                iTotalCredits += course.credits;
-            });
-
-            oModel.setProperty("/ui/totalCredits", iTotalCredits);
-
-            var iMin = oModel.getProperty("/student/minCredits");
-            var iMax = oModel.getProperty("/student/maxCredits");
-
-            var bWarningVisible = false;
-            var sWarningMessage = "";
-            var sWarningType = "None"; // Warning, Error, Success
-
-            if (iTotalCredits < iMin) {
-                bWarningVisible = true;
-                sWarningMessage = "Not enough Credit Hours! Minimum is " + iMin;
-                sWarningType = "Warning"; // Or Error depending on strictness
-            } else if (iTotalCredits > iMax) {
-                bWarningVisible = true;
-                sWarningMessage = "Credit Limit Exceeded! Maximum is " + iMax;
-                sWarningType = "Error";
-            } else {
-                bWarningVisible = false;
-            }
-
-            oModel.setProperty("/ui/isWarningVisible", bWarningVisible);
-            oModel.setProperty("/ui/warningMessage", sWarningMessage);
-            oModel.setProperty("/ui/warningType", sWarningType);
-        }
+        });
     });
-});
